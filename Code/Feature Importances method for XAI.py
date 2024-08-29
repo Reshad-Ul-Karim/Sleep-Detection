c3436b81a58ce37d988e2bc48c191a2b21bc986d
Reshad Ul Karim <reshad.steroid1@gmail.com>
2024-08-29 15:54:18 +0600

    feature importance pictures

--- Code/Feature Importances method for XAI.py
+++ Code/Feature Importances method for XAI.py
@@ -13,11 +13,11 @@ import seaborn as sns
 from sklearn.inspection import permutation_importance
 
 # Load the dataset
-df = pd.read_csv("Sleep_Stage_Combo2.csv")
+df = pd.read_csv("Sleep_Stage_Combo_main.csv")
 drop_columns = ['SubNo', "SegNo", "Class", 'averageTeagerEnergy', 'harmonicMean', 'svdPPI',
                 'averageTeagerEnergy_statistical', 'harmonicMean_statistical', 'svdPPG']
 X = df.drop(drop_columns, axis=1)
-y = df["Class2"]
+y = df["Class"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.30, random_state=150, stratify=y)
 
