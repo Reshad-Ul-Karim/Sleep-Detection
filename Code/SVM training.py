3ac9a5b0febe582e64293ea199185d9ce44c47ff
Reshad Ul Karim <reshad.steroid1@gmail.com>
2024-08-29 16:30:17 +0600

    SVM confusion matrix added

--- Code/SVM training.py
+++ Code/SVM training.py
@@ -1,12 +1,12 @@
-
-
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load the dataset again
 df = pd.read_csv("Sleep_Stage_Combo2.csv")
@@ -54,3 +54,13 @@ print(svm_class_report)
 with open("SVM model_evaluation.txt", "w") as file:
     file.write(f'Accuracy for best SVM: {svm_accuracy * 100:.2f}%\n')
     file.write(svm_class_report)
+
+# Generate and save the confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred_best_svm)
+plt.figure(figsize=(10, 7))
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=np.unique(y), yticklabels=np.unique(y))
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.title('Confusion Matrix for SVM')
+plt.savefig("svm_confusion_matrix.png")
+plt.show()
